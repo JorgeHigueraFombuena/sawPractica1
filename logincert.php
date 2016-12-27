050b9a53901c39207ef804efbd7917b891bc0ee5
Jorge Higuera <jorge.higuera.fombuena@gmail.com>
2016-12-27 21:22:45 +0100

    Added authentication with SSL

--- logincert.php
+++ logincert.php
@@ -1,6 +1,21 @@
 <?php
-	session_start();
-	// TODO 11
-	// Comprobar que el CN del certificado es un usuario en la BD.
-	
+session_start();
+$usuario = $_SERVER['SSL_CLIENT_S_DN_CN'];
+
+include("includes/abrirbd.php");
+$sql = "SELECT * FROM usuarios WHERE user ='{$usuario}'";
+$resultado = mysqli_query($link, $sql);
+
+if (mysqli_num_rows($resultado) == 1) {
+    $usuario = mysqli_fetch_assoc($resultado);
+    $_SESSION['autenticado'] = 'correcto';
+    $_SESSION['permisos'] = str_split($usuario['permisos']);
+    $_SESSION['user'] = $usuario['user'];
+    header("Location: MasterWeb.php");
+} else {
+    $_SESSION['autenticado'] = 'incorrecto';
+    header("Location: NoAuth.php");
+}
+
+mysqli_close($link);
 ?>
